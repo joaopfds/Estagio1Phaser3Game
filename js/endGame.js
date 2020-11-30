f57f12cf90c37eb823a84d78a861215203b2ef07
João Pedro Fructuoso <54864399+joaopfds@users.noreply.github.com>
2020-11-29 21:53:19 -0300

    Update endGame.js

--- js/endGame.js
+++ js/endGame.js
@@ -4,33 +4,14 @@ class EndGame extends Phaser.Scene
     constructor()
     {
         super("EndGame");
-        //this.mensagem;
     }
 
   
 
     create()
-    {
-        
-       // game.scene.keys["PlayGame"].fase=1;
-        
-        
-        
+    {    
         this.add.image(0,0,"home").setOrigin(0,0);
-        /*if(this.mensagem === "Você Venceu!!!")
-        {
-           
-            this.add.text(150,50,"Você venceu com o tempo de: " + game.scene.keys["PlayGame"].tempo, {fontSize:'22px', fill:'purple'});
-            if(localStorage.getItem("recorde") < game.scene.keys["PlayGame"].tempo)
-            {
-                localStorage.setItem("recorde", game.scene.keys["PlayGame"].tempo);
-                this.add.text(150,80,"Parabéns, você tem o novo recorde do jogo", {fontSize:'22px', fill:'purple'});
-            }
-        }
-        else
-        {
-            this.add.text(150,50,this.mensagem, {fontSize:'22px', fill:'purple'});
-        }*/
+        
         this.t = (120 - game.scene.keys["PlayGame"].tempo);
         if(game.scene.keys["PlayGame"].inimigos > 0){
             this.add.text(360,300,"Você falhou, tente novamente", {fontSize:'22px', fill:'purple', font: 'bold 25px Arial',backgroundColor: "gray",});
@@ -60,9 +41,6 @@ class EndGame extends Phaser.Scene
             fill: "#ffff",
           });
         btnHome.setInteractive();
-        
-        //game.scene.keys["PlayGame"].restat();
-        //game.scene.keys["PlayGame"].tempo=90;
 
         //Adicionar o clique do botao
         btnPlay.on("pointerdown", () => this.scene.start("PlayGame"));
